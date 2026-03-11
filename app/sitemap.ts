2357de08e6a785a531117853cb194a47c1b6e9b6
Alper <erkanckr4@gmail.com>
2026-03-11 22:39:46 +0300

    sitemap guncellendi

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,36 +1,29 @@
 export default function sitemap() {
+  const baseUrl = "https://hesapuzmani.site"
+
   return [
-    {
-      url: "https://www.hesapuzmani.site",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/kredi-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/kdv-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/yuzde-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/faiz-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/kira-artis-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/enflasyon-hesaplama",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.hesapuzmani.site/yas-hesaplama",
-      lastModified: new Date(),
-    },
+    { url: `${baseUrl}`, lastModified: new Date() },
+
+    { url: `${baseUrl}/yuzde-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/kredi-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/faiz-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/kdv-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/altin-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/dolar-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/maas-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/enflasyon-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/yas-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/kar-zarar-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/hisse-kar-zarar`, lastModified: new Date() },
+    { url: `${baseUrl}/kira-artis-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/arac-km-maliyeti`, lastModified: new Date() },
+    { url: `${baseUrl}/iki-tarih-arasi-gun-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/mevduat-faiz-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/temettu-hesaplama`, lastModified: new Date() },
+    { url: `${baseUrl}/tapu-harci-hesaplama`, lastModified: new Date() },
+
+    { url: `${baseUrl}/gizlilik-politikasi`, lastModified: new Date() },
+    { url: `${baseUrl}/kullanim-sartlari`, lastModified: new Date() },
+    { url: `${baseUrl}/iletisim`, lastModified: new Date() },
   ]
 }
