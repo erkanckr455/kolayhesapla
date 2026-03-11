9f1c0256148dca1793dc4a629f16bf89b0365d38
Alper <erkanckr4@gmail.com>
2026-03-11 04:08:24 +0300

    sitemap eklendi

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://kolayhesapla.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/kredi-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/kdv-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/yuzde-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/faiz-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/kira-artis-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/enflasyon-hesaplama",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolayhesapla.vercel.app/yas-hesaplama",
+      lastModified: new Date(),
+    },
+  ]
+}
